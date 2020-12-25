824bd912f15e1581bc1f75772c133af2c4ed22e9
Desmond Brown <browndesmond30@yahoo.com>
2020-12-25 22:45:37 +0000

    kali: Added Authentication in django backend project to handle all code related to authentication

---
+++ backend/authentication/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.validators import ValidationError
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class SignupSerializer(serializers.Serializer):
+
+    first_name = serializers.CharField(max_length=100, required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(trim_whitespace=True, required=True)
+
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        """
+        Create a user in the db and return it
+        :param validated_data:
+        :return:
+        """
+        data = validated_data
+        data["username"] = data["email"]
+        return User.objects.create_user(**data)
+
+    def validate_password(self, value):
+        """
+        Validate using django's default password validators
+        :param value:
+        :return:
+        """
+        try:
+            validate_password(value)
+        except ValidationError:
+            raise serializers.ValidationError(str(ValidationError))
+        return value
---
+++ backend/authentication/tests/test_serializers.py
@@ -0,0 +1,87 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework.test import APITestCase
+
+from authentication.serializers import SignupSerializer
+
+
+User = get_user_model()
+
+
+class TestSignupSerializer(APITestCase):
+    def setUp(self):
+        self.user_data = {
+            "first_name": "test",
+            "email": "test@email.com",
+            "password": "valid@a1b2c3iq"
+        }
+
+        self.RequiredError = "This field is required."
+
+    def test_serializer_is_true_if_valid_data(self):
+        """
+        Test that serializer.is_valid() returns True for valid user data
+        :return:
+        """
+        serializer = SignupSerializer(data=self.user_data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_serializer_is_false_if_no_first_name_in_user_data(self):
+        """
+        Test that serializer.is_valid() returns False if no first_name key in
+        user request json data
+        :return:
+        """
+        data = self.user_data
+        del data["first_name"]
+        serializer = SignupSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(serializer.errors['first_name'][0], self.RequiredError)
+
+    def test_serializer_is_false_if_no_email_in_user_data(self):
+        """
+        Test that serializer.is_valid() returns False if no email key in user
+        request json data
+        :return:
+        """
+        data = self.user_data
+        del data["email"]
+        serializer = SignupSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(serializer.errors["email"][0], self.RequiredError)
+
+    def test_serializer_is_false_if_no_password_in_user_data(self):
+        """
+        Test that serializer.is_valid() returns False if no password key in user
+        request json data
+        :return:
+        """
+        data = self.user_data
+        del data["password"]
+        serializer = SignupSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(serializer.errors["password"][0], self.RequiredError)
+
+    def test_serializer_creates_new_user_if_valid_user_data(self):
+        """
+        Tests that serializer.save() creates a new User instance in the db if
+        user request json is valid
+        :return:
+        """
+        serializer = SignupSerializer(data=self.user_data)
+        serializer.is_valid()
+        user = serializer.save()
+        self.assertEqual(user, User.objects.all()[0])
+
+    def test_serializer_does_not_create_new_user_if_invalid_user_data(self):
+        """
+        Tests that serializer.save() does not create a new User instance in the db
+        if user request json is invalid
+        :return:
+        """
+        data = self.user_data
+        del data["first_name"]
+        serializer = SignupSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        with self.assertRaises(AssertionError):
+            serializer.save()
