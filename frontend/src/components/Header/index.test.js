9ef9d680d6ca9f99af98ed1fe2bae5815af2d8c6
Desmond Brown <browndesmond30@yahoo.com>
2020-12-25 18:57:04 +0000

    Kali: Added Test Configuration for frontend (React) with Jest and Enzyme and currently dummy testing the Header component

---
+++ frontend/src/components/Header/index.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import Header from './index'
+
+
+const setUp = (props = {}) => {
+    return shallow(<Header {...props}/>)
+}
+
+
+describe('Header Component Unit Testing', () => {
+    let component;
+    beforeEach(() => {
+        component = setUp()
+    })
+
+    it('Should render well without any errors', () => {
+        const wrapper = component.find(".header")
+        expect(wrapper.length).toBe(1)
+    })
+
+    it('Should render one (1) logo image', () => {
+        const wrapper = component.find("[data-test='logoImg']")
+        expect(wrapper.length).toBe(1)
+    })
+
+    it('Should render with 4 links', () => {
+        const wrapper = component.find('li NavLink')
+        expect(wrapper.length).toBe(4)
+    })
+})
