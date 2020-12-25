287a655bce74bf1b9118f1dc3e2f51da223752da
Desmond Brown <browndesmond30@yahoo.com>
2020-12-25 12:52:38 +0000

    Kali: Reorganized folder structure of project

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+// External library imports here
+import {NavLink, Link} from 'react-router-dom'
+
+// Assets Here
+import logo from '../../assets/img/logo.png'
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav className="navbar navbar-expand-lg header-nav">
+                <div className="navbar-header">
+                    <NavLink to={"#"} id="mobile_btn">
+							<span className="bar-icon">
+								<span></span>
+								<span></span>
+								<span></span>
+							</span>
+                    </NavLink>
+                    <NavLink to={'/'} className="navbar-brand logo">
+                        <img src={logo} className="img-fluid" alt="Logo"/>
+                    </NavLink>
+                </div>
+                <div className="main-menu-wrapper">
+                    <div className="menu-header">
+                        <NavLink to={"#"} className="menu-logo">
+                            <img src={logo} className="img-fluid" alt="Logo"/>
+                        </NavLink>
+                        <NavLink to={"#"} id="menu_close" className="menu-close">
+                            <i className="fas fa-times"></i>
+                        </NavLink>
+                    </div>
+                    <ul className="main-nav">
+                        <li>
+                            <NavLink to={"/"}>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={"#"}>Patients</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to={"/"} target="_blank">Admin</NavLink>
+                        </li>
+                        <li className="login-link">
+                            <NavLink to={"/"}>Login / Signup</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <ul className="nav header-navbar-rht">
+                    <li className="nav-item contact-item">
+                        <div className="header-contact-img">
+                            <i className="far fa-hospital"/>
+                        </div>
+                        <div className="header-contact-detail">
+                            <p className="contact-header">Contact</p>
+                            <p className="contact-info-header"> +1 315 369 5943</p>
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link header-login" to={"#"}>login / Signup </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+
+export default Header;
