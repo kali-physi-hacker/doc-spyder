e0c9dee6b7380e59bcce87517583f3c759444744
Desmond Brown <browndesmond30@yahoo.com>
2020-12-26 10:19:13 +0000

    Kali: Implemented logout that just deletes the Authorization key on the cookie and implemented significant test

---
+++ backend/authentication/controllers/logout.py
@@ -0,0 +1,17 @@
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class Logout(GenericViewSet):
+
+    def logout(self, request):
+        """
+        Delete the authorization cookie
+        :param request:
+        :return:
+        """
+
+        response = Response({"success": True}, status=status.HTTP_200_OK)
+        response.delete_cookie("Authorization")
+        return response
---
+++ backend/authentication/tests/test_views_logout.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APITestCase, APIClient
+
+User = get_user_model()
+
+
+class TestLogout(APITestCase):
+    def setUp(self):
+        self.password = "test@a1b2c3iq"
+        self.user = User.objects.create_user(username="test@email.com", password=self.password)
+
+        self.client = APIClient()
+
+    def test_logout_deletes_authorization_cookie_value(self):
+        """
+        Tests that logout deletes authorization cookie value
+        :return:
+        """
+        response = self.client.post(reverse("authentication:login"), {"email": self.user.username, "password": self.password})
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse("authentication:logout"))
+        self.assertEqual(response.cookies["Authorization"].value, "")
